945f299b9d0d800ac64f440e06f691aea9023724
zuggernautt <amitak0707@gmail.com>
2022-07-31 16:04:49 +0530

    made model class and hooked up with admin

--- ToDo/List/models.py
+++ ToDo/List/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    TaskInfo = models.CharField(max_length=50,
+    help_text = 'Enter the task details in brief')
+
+    def __str__(self):
+        return self.TaskInfo
+
+
+
